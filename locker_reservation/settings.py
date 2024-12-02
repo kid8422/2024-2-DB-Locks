c0e8b5f6c5123fb3f601b64b2b17e335887e0091
haechan8422 <kid8422@naver.com>
2024-12-02 15:19:23 +0900

    [Locks] Debug off

--- locker_reservation/settings.py
+++ locker_reservation/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-no=*&^*ukq@dw#)l=lqddak=qz&3sz82de^6^aaxm9xk1*k(1@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
