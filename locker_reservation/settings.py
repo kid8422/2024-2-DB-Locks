5f6b0ee8cbc3a5a2c88cd591ae1d5c7c3a614013
haechan8422 <kid8422@naver.com>
2024-12-02 15:56:17 +0900

    .

--- locker_reservation/settings.py
+++ locker_reservation/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-no=*&^*ukq@dw#)l=lqddak=qz&3sz82de^6^aaxm9xk1*k(1@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['svc.sel4.cloudtype.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
